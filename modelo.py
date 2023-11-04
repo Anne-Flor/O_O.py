5e191f246a36932ecf399e203cc1d22a6428be23
Anne-flor <annygabrielly730@gmail.com>
2023-11-04 18:45:22 -0300

    Criando def imprime

--- modelo.py
+++ modelo.py
@@ -18,31 +18,35 @@ class Programa():
     @titulo.setter
     def titulo(self, novo_titulo):
         self._titulo = novo_titulo.title()
+    
+    def imprime(self):
+        print(f'{self._titulo} - {self.ano} - Likes {self._likes}')
 
 class Filme(Programa):
     def __init__(self, titulo, ano, duracao):
         super().__init__(titulo, ano) #chamando o inicializador da classe mãe, o meu objeto recebe a instacia que foi criada pela classe mãe
         self.duracao   = duracao
 
+    def imprime(self):
+        print(f'{self._titulo} - Minutos : {self.duracao} - {self.ano} - Likes {self._likes}')
+
 class Series(Programa):
     def __init__(self, titulo, ano, temporadas):
         super().__init__(titulo, ano)
-        self.temporadas   = temporadas 
+        self.temporadas   = temporadas
+
+    def imprime(self):
+        print(f'{self._titulo} - Temporadas : {self.temporadas} - {self.ano} - Likes {self._likes}')
+        
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
 atlanta = Series('atlanta', 2018, 2)
-vingadores.dar_like()
-vingadores.dar_like()
-vingadores.dar_like()
 
 atlanta.dar_like()
 atlanta.dar_like()
-
-print(f" Nome {vingadores.titulo} - {vingadores.ano} - {vingadores.duracao} - Likes {vingadores.likes}")
-print(f' Nome {atlanta.titulo} - {atlanta.ano} - {atlanta.temporadas} - Likes {atlanta.likes}')
 
 filmes_e_series = [vingadores, atlanta]
 
-for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.titulo} - D {detalhes} - {programa.likes}')
+for programa in filmes_e_series: 
+    programa.imprime()
