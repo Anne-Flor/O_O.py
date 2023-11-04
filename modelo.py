296f83c0a69157bbd73f4d1a22a94187ace42c4e
Anne-flor <annygabrielly730@gmail.com>
2023-11-04 18:57:13 -0300

    Criando uma representação textual para o def imprime

--- modelo.py
+++ modelo.py
@@ -19,24 +19,24 @@ class Programa():
     def titulo(self, novo_titulo):
         self._titulo = novo_titulo.title()
     
-    def imprime(self):
-        print(f'{self._titulo} - {self.ano} - Likes {self._likes}')
+    def __str__(self):
+        return f'{self._titulo} - {self.ano} - Likes {self._likes}'
 
 class Filme(Programa):
     def __init__(self, titulo, ano, duracao):
         super().__init__(titulo, ano) #chamando o inicializador da classe mãe, o meu objeto recebe a instacia que foi criada pela classe mãe
         self.duracao   = duracao
 
-    def imprime(self):
-        print(f'{self._titulo} - Minutos : {self.duracao} - {self.ano} - Likes {self._likes}')
+    def __str__(self):
+        return f'{self._titulo} - Minutos : {self.duracao} - {self.ano} - Likes {self._likes}'
 
 class Series(Programa):
     def __init__(self, titulo, ano, temporadas):
         super().__init__(titulo, ano)
         self.temporadas   = temporadas
 
-    def imprime(self):
-        print(f'{self._titulo} - Temporadas : {self.temporadas} - {self.ano} - Likes {self._likes}')
+    def __str__(self):
+        return f'{self._titulo} - Temporadas : {self.temporadas} - {self.ano} - Likes {self._likes}'
         
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -49,4 +49,4 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series: 
-    programa.imprime()
+    print(programa)
